3e5b1dddea3193d18a713b502c9700367feba0ba
Joe <joe831216@gmail.com>
2019-03-10 00:02:42 +0800

    Initial commit, this library now allows the users tags the service dependency information to create the service-dependency extension for SpringFox swagger.

---
+++ src/main/java/com/soselab/mgpmicroserviceclientservice/mgp/serivceclient/annotation/FeignRequest.java
@@ -0,0 +1,13 @@
+package com.soselab.mgpmicroserviceclientservice.mgp.serivceclient.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FeignRequest {
+    Class<?> client();
+    String method();
+}
---
+++ src/main/java/com/soselab/mgpmicroserviceclientservice/mgp/serivceclient/annotation/RabbitPublisher.java
@@ -0,0 +1,12 @@
+package com.soselab.mgpmicroserviceclientservice.mgp.serivceclient.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RabbitPublisher {
+    String value();
+}
